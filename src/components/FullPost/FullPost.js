417edcc94f8a627705f9c405106ec8546f657e17
Edy <maslesaeldin@gmail.com>
2020-03-27 23:19:45 +0100

    http request / axios, error handling

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+    state ={
+        loadedPost: null
+    }
+
+    //na ovaj nacin imamo beskonacnu petlju, jer se state updatuje, zatim se komponenta reloadira, zatim componentDidUpdate pokrene i sve tako
+    //BESKONACNA PETLJA
+    /*componentDidUpdate(){
+        if(this.props.id){
+            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                this.setState({loadedPost: response.data})
+            })
+        }
+    }*/
+
+    componentDidUpdate(){
+        //Ako imamo id od posta
+        if(this.props.id){
+            //Ako imamo loaded post, ili, ako loadedPost.id je razlicit od onoga sto smo dobili iz Blog
+            //komponente onda poalji zahtjev
+            if(!this.state.loadedPost || (this.state.loadedPost.id !== this.props.id)){
+                console.log(Boolean(!this.state.loadedPost))
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                this.setState({loadedPost: response.data})
+            })
+            }
+        }
+       
+    }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response)
+            })
+    }
+
+    render () {
+        // defaultno imamo da izabere post
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        // onda ako imamo id od posta pojavi se loading
+        if(this.props.id){
+            post = <p style={{textAlign: 'center'}}>Loading....</p>
+        }
+        // nakon sto smo dohvtili podatke sa servera ispisujemo post a uklanjamo loading
+        if(this.state.loadedPost){
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+       
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,64 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state ={
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount(){
+        //state moramo updatovati unutar axios metode, inace ne bi dobili podatke
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            //Dohvatili smo samo 4 posta sa servera
+            const posts = response.data.slice(0, 4);
+            //objekat koji smo dohvatili sa servera smo spreadali i dodali smo novi property author
+            const updatedPost = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Eldin'
+                }
+            })
+            this.setState({posts: updatedPost})
+            console.log(response)
+        })
+        .catch(error => {
+            //console.log(error)
+            this.setState({error: true})
+        })
+    }
+
+    postSelectedHandler = (id) => {
+        //Smjestili smo id od posta u selectedPostId state
+        this.setState({selectedPostId: id})
+    }
+
     render () {
+        let posts = <h5 style={{textAlign: 'center'}}>Something Went Wrong</h5>
+
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+                )
+            })
+        }
+
+        
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
